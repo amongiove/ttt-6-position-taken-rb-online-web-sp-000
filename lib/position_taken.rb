2470e65cacdb52dc1c33029916ac981b85130cd7
Amanda Mongiove <amandamongiove@gmail.com>
2020-08-30 01:45:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+board = [" "," "," "," "," "," "," "," "," "]
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  else
+    true
+  end
+end
+
+   
